b4983d61672beb0c7c8b15c651fc31dfb479d26a
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2024-01-11 11:41:10 +0600

    Driver Setup

--- src/test/java/org/example/DriverSetup.java
+++ src/test/java/org/example/DriverSetup.java
@@ -3,19 +3,21 @@ package org.example;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
 
 public class DriverSetup {
 
     public WebDriver browser;
 
-    @BeforeMethod
+    @BeforeTest
     public void openABrowser(){
         browser = new ChromeDriver();
         browser.manage().window().maximize();
     }
 
-    @AfterMethod
+    @AfterTest
     public void quiteBrowser(){
         browser.quit();
     }
