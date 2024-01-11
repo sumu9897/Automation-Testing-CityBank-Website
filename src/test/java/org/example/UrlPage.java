fdcd6c9cb1da7a7ee506953eb7d99388bb15eb98
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2024-01-11 11:38:51 +0600

    City Bank Automation Testing By Selenium

---
+++ src/test/java/org/example/UrlPage.java
@@ -0,0 +1,237 @@
+package org.example;
+
+import org.testng.annotations.Test;
+
+public class UrlPage extends DriverSetup {
+    @Test(priority = 0 )
+    public void OpenHome() throws InterruptedException {
+        browser.get("https://www.thecitybank.com/home");
+        Thread.sleep(2000);
+
+        System.out.println(browser.getTitle());
+        System.out.println(browser.getCurrentUrl());
+//        System.out.println(browser.getPageSource());
+        System.out.println(browser.getWindowHandle());
+        System.out.println(browser.getWindowHandles());
+        System.out.println(browser.switchTo());
+        System.out.println(browser.manage());
+        System.out.println(browser.navigate());
+    }
+
+    @Test(priority = 1 )
+    public void OpenAboutUs() throws InterruptedException {
+        browser.get("https://www.thecitybank.com/home#about-us");
+        Thread.sleep(2000);
+
+        System.out.println(browser.getTitle());
+        System.out.println(browser.getCurrentUrl());
+//        System.out.println(browser.getPageSource());
+        System.out.println(browser.getWindowHandle());
+        System.out.println(browser.getWindowHandles());
+        System.out.println(browser.switchTo());
+        System.out.println(browser.manage());
+        System.out.println(browser.navigate());
+    }
+
+    @Test (priority = 2 )
+    public void OpenAccountSubMenu() throws InterruptedException {
+        browser.get("https://www.thecitybank.com/home#account-sub-menu");
+        Thread.sleep(2000);
+
+        System.out.println(browser.getTitle());
+        System.out.println(browser.getCurrentUrl());
+//        System.out.println(browser.getPageSource());
+        System.out.println(browser.getWindowHandle());
+        System.out.println(browser.getWindowHandles());
+        System.out.println(browser.switchTo());
+        System.out.println(browser.manage());
+        System.out.println(browser.navigate());
+    }
+
+    @Test (priority = 3 )
+    public void OpenLoanSubMenu() throws InterruptedException {
+        browser.get("https://www.thecitybank.com/home#loan-submenu");
+        Thread.sleep(2000);
+
+        System.out.println(browser.getTitle());
+        System.out.println(browser.getCurrentUrl());
+//        System.out.println(browser.getPageSource());
+        System.out.println(browser.getWindowHandle());
+        System.out.println(browser.getWindowHandles());
+        System.out.println(browser.switchTo());
+        System.out.println(browser.manage());
+        System.out.println(browser.navigate());
+    }
+    @Test (priority = 4 )
+    public void OpenCardSubMenu() throws InterruptedException {
+        browser.get("https://www.thecitybank.com/home#card-submenu");
+        Thread.sleep(2000);
+
+        System.out.println(browser.getTitle());
+        System.out.println(browser.getCurrentUrl());
+//        System.out.println(browser.getPageSource());
+        System.out.println(browser.getWindowHandle());
+        System.out.println(browser.getWindowHandles());
+        System.out.println(browser.switchTo());
+        System.out.println(browser.manage());
+        System.out.println(browser.navigate());
+    }
+
+    @Test (priority = 5 )
+    public void OpenDigitalBanking() throws InterruptedException {
+        browser.get("https://www.thecitybank.com/home#digital-banking");
+        Thread.sleep(2000);
+
+        System.out.println(browser.getTitle());
+        System.out.println(browser.getCurrentUrl());
+//        System.out.println(browser.getPageSource());
+        System.out.println(browser.getWindowHandle());
+        System.out.println(browser.getWindowHandles());
+        System.out.println(browser.switchTo());
+        System.out.println(browser.manage());
+        System.out.println(browser.navigate());
+    }
+
+    @Test (priority = 6 )
+    public void OpenWholesaleSubMenu() throws InterruptedException {
+        browser.get("https://www.thecitybank.com/home#wholesale-submenu");
+        Thread.sleep(2000);
+
+        System.out.println(browser.getTitle());
+        System.out.println(browser.getCurrentUrl());
+//        System.out.println(browser.getPageSource());
+        System.out.println(browser.getWindowHandle());
+        System.out.println(browser.getWindowHandles());
+        System.out.println(browser.switchTo());
+        System.out.println(browser.manage());
+        System.out.println(browser.navigate());
+    }
+
+    @Test (priority = 7 )
+    public void OpenTreasury() throws InterruptedException {
+        browser.get("https://www.thecitybank.com/account/freelancers-solutions#treasury-submenu");
+        Thread.sleep(2000);
+
+        System.out.println(browser.getTitle());
+        System.out.println(browser.getCurrentUrl());
+//        System.out.println(browser.getPageSource());
+        System.out.println(browser.getWindowHandle());
+        System.out.println(browser.getWindowHandles());
+        System.out.println(browser.switchTo());
+        System.out.println(browser.manage());
+        System.out.println(browser.navigate());
+    }
+
+
+    @Test (priority = 8 )
+    public void OpenPriorityBanking() throws InterruptedException {
+        browser.get("https://www.thecitybank.com/account/freelancers-solutions#prioritybanking");
+        Thread.sleep(2000);
+
+        System.out.println(browser.getTitle());
+        System.out.println(browser.getCurrentUrl());
+//        System.out.println(browser.getPageSource());
+        System.out.println(browser.getWindowHandle());
+        System.out.println(browser.getWindowHandles());
+        System.out.println(browser.switchTo());
+        System.out.println(browser.manage());
+        System.out.println(browser.navigate());
+    }
+
+
+    @Test (priority = 9 )
+    public void OpenAmericanExpress() throws InterruptedException {
+        browser.get("https://www.americanexpress.com/en-bd/network/");
+        Thread.sleep(2000);
+
+        System.out.println(browser.getTitle());
+        System.out.println(browser.getCurrentUrl());
+//        System.out.println(browser.getPageSource());
+        System.out.println(browser.getWindowHandle());
+        System.out.println(browser.getWindowHandles());
+        System.out.println(browser.switchTo());
+        System.out.println(browser.manage());
+        System.out.println(browser.navigate());
+    }
+
+
+    @Test (priority = 10 )
+    public void OpenSMEBanking() throws InterruptedException {
+        browser.get("https://www.thecitybank.com/loan/city-sme-small-loan");
+        Thread.sleep(2000);
+
+        System.out.println(browser.getTitle());
+        System.out.println(browser.getCurrentUrl());
+//        System.out.println(browser.getPageSource());
+        System.out.println(browser.getWindowHandle());
+        System.out.println(browser.getWindowHandles());
+        System.out.println(browser.switchTo());
+        System.out.println(browser.manage());
+        System.out.println(browser.navigate());
+    }
+
+
+    @Test (priority = 11 )
+    public void OpenWomenBaning() throws InterruptedException {
+        browser.get("https://cityalo.com/");
+        Thread.sleep(2000);
+
+        System.out.println(browser.getTitle());
+        System.out.println(browser.getCurrentUrl());
+//        System.out.println(browser.getPageSource());
+        System.out.println(browser.getWindowHandle());
+        System.out.println(browser.getWindowHandles());
+        System.out.println(browser.switchTo());
+        System.out.println(browser.manage());
+        System.out.println(browser.navigate());
+    }
+    @Test (priority = 12 )
+    public void OpenEmployeeBanking() throws InterruptedException {
+        browser.get("https://www.thecitybank.com/solutions/employee-banking");
+        Thread.sleep(2000);
+
+        System.out.println(browser.getTitle());
+        System.out.println(browser.getCurrentUrl());
+//        System.out.println(browser.getPageSource());
+        System.out.println(browser.getWindowHandle());
+        System.out.println(browser.getWindowHandles());
+        System.out.println(browser.switchTo());
+        System.out.println(browser.manage());
+        System.out.println(browser.navigate());
+    }
+
+    @Test (priority = 13 )
+    public void OpenAgentBanking() throws InterruptedException {
+        browser.get("https://www.thecitybank.com/agent-banking");
+        Thread.sleep(2000);
+
+        System.out.println(browser.getTitle());
+        System.out.println(browser.getCurrentUrl());
+//        System.out.println(browser.getPageSource());
+        System.out.println(browser.getWindowHandle());
+        System.out.println(browser.getWindowHandles());
+        System.out.println(browser.switchTo());
+        System.out.println(browser.manage());
+        System.out.println(browser.navigate());
+    }
+
+    @Test (priority = 14 )
+    public void OpenFinancialLiteracy() throws InterruptedException {
+        browser.get("https://www.thecitybank.com/financial-literacy");
+        Thread.sleep(2000);
+
+        System.out.println(browser.getTitle());
+        System.out.println(browser.getCurrentUrl());
+//        System.out.println(browser.getPageSource());
+        System.out.println(browser.getWindowHandle());
+        System.out.println(browser.getWindowHandles());
+        System.out.println(browser.switchTo());
+        System.out.println(browser.manage());
+        System.out.println(browser.navigate());
+    }
+
+
+
+
+
+}
