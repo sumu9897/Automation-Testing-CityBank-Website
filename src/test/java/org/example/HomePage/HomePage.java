47a43cbeda825d14db365c7fa687c2fa172ca383
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2024-01-26 11:33:57 +0600

    Home Page

--- src/test/java/org/example/HomePage/HomePage.java
+++ src/test/java/org/example/HomePage/HomePage.java
@@ -4,20 +4,33 @@ import org.example.DriverSetup;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Test;
 
 public class HomePage extends DriverSetup {
-    @Test
-    public void OpenHome() throws InterruptedException {
+
+
+    @Test (priority = 0 )
+    public  void GetStarted() throws InterruptedException {
+        browser.get("https://www.thecitybank.com/home");
+        Thread.sleep(1000);
+
+        WebElement dropdown = browser.findElement(By.id("who"));
+        Select select = new Select(dropdown);
+        select.selectByValue("7");
+        Thread.sleep(1000);
+    }
+
+
+    @Test (priority = 1 )
+    public void CurrencyConverter() throws InterruptedException {
         browser.get("https://www.thecitybank.com/home");
         Thread.sleep(2000);
 
-        JavascriptExecutor js =(JavascriptExecutor) browser;
-        js.executeScript("window.scrollTo(0, document.body.scrollHeight);");
-        Thread.sleep(3000);
+        browser.findElement(By.name("amount_usd")).sendKeys("11");
+        Thread.sleep(2000);
 
-        js.executeScript("window.scrollTo(document.body.scrollHeight, 0);");
-        Thread.sleep(3000);
+
 
 
     }
