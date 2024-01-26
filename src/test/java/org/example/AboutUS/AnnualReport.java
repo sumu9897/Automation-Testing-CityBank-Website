9c4474c1eb2c260152ac00c755968a72de0bd3d1
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2024-01-26 12:04:27 +0600

    About Us

--- src/test/java/org/example/AboutUS/AnnualReport.java
+++ src/test/java/org/example/AboutUS/AnnualReport.java
@@ -1,4 +1,28 @@
 package org.example.AboutUS;
 
-public class AnnualReport {
+import org.example.DriverSetup;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+public class AnnualReport extends DriverSetup {
+
+
+    @Test
+    public void testScroll() throws InterruptedException {
+        browser.get("https://www.citybankplc.com/report/annualreports");
+
+        JavascriptExecutor js =(JavascriptExecutor) browser;
+        js.executeScript("window.scrollTo(0, document.body.scrollHeight);");
+        Thread.sleep(3000);
+
+        js.executeScript("window.scrollTo(document.body.scrollHeight, 0);");
+        Thread.sleep(3000);
+
+        WebElement elementToClick = browser.findElement(By.xpath("//div[@class='annual-cards items']//div[1]//div[1]//div[2]//a[1]"));
+        elementToClick.click();
+        Thread.sleep(5000);
+
+    }
 }
--- src/test/java/org/example/AboutUS/Awards.java
+++ src/test/java/org/example/AboutUS/Awards.java
@@ -1,4 +1,20 @@
 package org.example.AboutUS;
 
-public class Awards {
+import org.example.DriverSetup;
+import org.openqa.selenium.JavascriptExecutor;
+import org.testng.annotations.Test;
+
+public class Awards extends DriverSetup {
+
+    @Test
+    public void testScroll() throws InterruptedException {
+        browser.get("https://www.citybankplc.com/awards-accolades");
+
+        JavascriptExecutor js =(JavascriptExecutor) browser;
+        js.executeScript("window.scrollTo(0, document.body.scrollHeight);");
+        Thread.sleep(3000);
+
+        js.executeScript("window.scrollTo(document.body.scrollHeight, 0);");
+        Thread.sleep(3000);
+    }
 }
